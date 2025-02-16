a7a1d9cd3cbff13beeccf8abc3d6a1b7a40d86bb
LIUCBK <liucbk@bc.edu>
2025-02-16 15:13:51 -0500

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by 刘佳雨 on 2025/2/16.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
